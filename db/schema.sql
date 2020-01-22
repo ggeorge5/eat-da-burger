eb85adbcbdd9bf75f2bac3d6928d414f83674217
ggeorge <gmgeorge1993@gmail.com>
2020-01-21 21:36:13 -0600

    added some things

--- db/schema.sql
+++ db/schema.sql
@@ -3,10 +3,9 @@ USE burga_db;
 
 DROP TABLE IF EXISTS burgers;
 
--- Create the burgers table
 CREATE TABLE burgers (
-    id int NOT NULL AUTO_INCREMENT,
-    burger_name varchar(255) NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR(255) NOT NULL,
     devoured BOOLEAN DEFAULT false,
     PRIMARY KEY (id)
 );
